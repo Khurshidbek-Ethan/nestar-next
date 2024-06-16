f732b39337362a12aca05d562104dfba2aeb1867
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-06-16 19:14:05 +0900

    fix:modify CommunityBoards component

--- libs/components/homepage/CommunityBoards.tsx
+++ libs/components/homepage/CommunityBoards.tsx
@@ -4,6 +4,10 @@ import useDeviceDetect from '../../hooks/useDeviceDetect';
 import { Stack, Typography } from '@mui/material';
 import CommunityCard from './CommunityCard';
 import { BoardArticle } from '../../types/board-article/board-article';
+import { GET_BOARD_ARTICLES } from '../../../apollo/user/query';
+import { T } from '../../types/common';
+import { useQuery } from '@apollo/client';
+import { BoardArticleCategory } from '../../enums/board-article.enum';
 
 const CommunityBoards = () => {
 	const device = useDeviceDetect();
@@ -16,6 +20,33 @@ const CommunityBoards = () => {
 	const [freeArticles, setFreeArticles] = useState<BoardArticle[]>([]);
 
 	/** APOLLO REQUESTS **/
+	const {
+		loading: getNewsArticlesLoading,
+		data: getNewsArticlesData,
+		error: getNewsArticlesError,
+		refetch: getNewsArticlesRefetch,
+	} = useQuery(GET_BOARD_ARTICLES, {
+		fetchPolicy: 'network-only', // cach + =>network
+		variables: { input: { ...searchCommunity, limit: 6, search: { articleCategory: BoardArticleCategory.NEWS } } },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setNewsArticles(data?.getBoardArticles?.list);
+		},
+	});
+
+	const {
+		loading: getFreeArticlesLoading,
+		data: getFreeArticlesData,
+		error: geFreeArticlesError,
+		refetch: getFreeArticlesRefetch,
+	} = useQuery(GET_BOARD_ARTICLES, {
+		fetchPolicy: 'network-only', // cach + =>network
+		variables: { input: { ...searchCommunity, limit: 3, search: { articleCategory: BoardArticleCategory.FREE } } },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setFreeArticles(data?.getBoardArticles?.list);
+		},
+	});
 
 	if (device === 'mobile') {
 		return <div>COMMUNITY BOARDS (MOBILE)</div>;

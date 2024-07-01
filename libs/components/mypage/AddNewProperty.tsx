598371bd070d6ba5b46a7d0d057b95cd5421c2bb
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-07-02 01:00:40 +0900

    fix:I checked  AllPropertiesInquiry

--- libs/components/mypage/AddNewProperty.tsx
+++ libs/components/mypage/AddNewProperty.tsx
@@ -208,7 +208,7 @@ const AddProperty = ({ initialValues, ...props }: any) => {
 								<Stack className="price-year-after-price">
 									<Typography className="title">Price</Typography>
 									<input
-										type="text"
+										type="number"
 										className="description-input"
 										placeholder={'Price'}
 										value={insertPropertyData.propertyPrice}
